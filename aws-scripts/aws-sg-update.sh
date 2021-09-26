0bd71e34d04733220b14960d1e5d51a47f02f417
0ccupi3R <47894266+0ccupi3R@users.noreply.github.com>
2021-09-26 15:01:02 +0530

    Update aws-sg-update.sh

--- aws-scripts/aws-sg-update.sh
+++ aws-scripts/aws-sg-update.sh
@@ -4,16 +4,27 @@
 # 
 # Protocol  - all/tcp/udp
 # Port      - 80/80-443/all
-# Profile   - dev/security/stage/prod
+# Profile   - dev/stage/prod [optional]
 
+#!/bin/bash
+BOLD=`tput bold`
 RED=`tput setaf 1`
 YELLOW=`tput setaf 3`
+DIM=`tput dim`
 RESET=`tput sgr0`
 
-if [ $# -eq 3 ]
+if [ $# -eq 2 ]
 then
+    echo "Without Profile"
     ip=$(dig +short myip.opendns.com @resolver1.opendns.com.)
-    aws ec2 authorize-security-group-ingress --group-name shahnawaz-home --protocol $1 --port $2 --cidr $ip/32 --profile $3
+    aws ec2 authorize-security-group-ingress --group-name home-router --protocol $1 --port $2 --cidr $ip/32
 else
-    echo "${YELLOW}Please provide three arguments -${RESET} ${RED}aws-sg-update.sh <tcp/udp/all> <80/80-443/all> <dev/security/prod>${RESET}"
+    if [ $# -eq 3 ]
+    then
+        echo "With Profile"
+        ip=$(dig +short myip.opendns.com @resolver1.opendns.com.)
+        aws ec2 authorize-security-group-ingress --group-name home-router --protocol $1 --port $2 --cidr $ip/32 --profile $3
+    else
+            echo "${BOLD}${RED}Please provide the arguments [ protocol, port number, profile (optional) ]${RESET}\n${YELLOW}${DIM}aws-sg-update.sh <tcp/udp/all> <80/80-443/all> <dev/stage/prod>${RESET}\nExample: aws-sg-update.sh tcp 80 dev${RESET}\n"
+    fi
 fi
